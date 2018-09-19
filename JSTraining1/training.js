7e8752d89b8a4bdc037d8c71347bdb76c162680b
LucasNasc <lucaspnascimento@hotmail.com>
2018-09-18 23:50:31 -0300

    Commit Inicial

---
+++ JSTraining1/training.js
@@ -0,0 +1,27 @@
+function ApertouMouse() {
+
+    console.log("Apertou o Mouse!");
+}
+function SoltouMouse() {
+
+    console.log("Soltou Mouse");
+}
+function OnMouseOver() {
+
+    console.log("mouse em cima");
+}
+function OnMouseOut() {
+    console.log ("mouse fora");
+}
+function MoveuMouse(){
+    console.log("mouse se moveu");
+}
+function clicou() {
+    console.log("clicou");
+}
+function BotaoDireito() {
+
+    console.log("botao direito");
+
+    return false;
+}
