c6246b36af3ec15d0a8a4a9f45bf0e28fd706bfd
LucasNasc <lucaspnascimento@hotmail.com>
2018-09-26 22:40:57 -0300

    Atualização Jogo

--- JsGameBaloon/script.js
+++ JsGameBaloon/script.js
@@ -1,11 +1,16 @@
 function addBaloon() {
     baloon = document.createElement("div");
     baloon.setAttribute("class", "bola");
+    redRandom = Math.floor(Math.random() * 250 )
+    blueRandom = Math.floor(Math.random() * 250 )
+    greenRandom = Math.floor(Math.random() * 250 )
+    
+    
 
     p1 = Math.floor((Math.random() + 0.5  ) * 500 );
     p2 = Math.floor((Math.random() + 0.5  ) * 400 );
 
-    baloon.setAttribute("style", "left: "+ p1 + "px; top: " +p2+"px;");
+    baloon.setAttribute("style", "left: "+ p1 + "px; top: " +p2+"px; background-color: rgb("+redRandom+","+greenRandom+","+blueRandom+");");
     baloon.setAttribute("onclick", "estourar(this)");
 
     document.body.appendChild(baloon);
@@ -15,11 +20,14 @@ var pontos = 0
 function estourar(elemento) {
 
     document.body.removeChild(elemento);
-   
+    pontos++
+    document.getElementById('pontos').innerHTML = 'Pontuacao: '+ pontos;
     
+
     
 }
 
 function iniciar(){
-    setInterval(addBaloon, 1000);
+    setInterval(addBaloon, 1500);
+    
 }
